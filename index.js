72a826b0c56f2c8374b61728ea1b01a5ca7344f0
kardelio <kardeliogithub@outlook.com>
2020-10-05 14:08:48 +0200

    Added 2 new functions and 2 news tests (level 5 & 6)

--- index.js
+++ index.js
@@ -12,27 +12,41 @@ function helloWorld() {
 // Level 2
 // Return `hello ${name}`
 function helloName(name) {
-  return "hello";
+    return "hello";
 }
 
 // Level 3
 // Return "hello world"
 function helloWorldFromArray() {
-  return ['h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'l', 'd'].join("");
+    return ['h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'l', 'd'].join("");
 }
 
 // Level 4
 // Fix this function
 function doStuffWithVariable(variable) {
-  let a = wrongVariable + 1;
-  let b = wrongVariable + 2;
-  let c = wrongVariable + 3;
-  return a + b + c;
+    let a = wrongVariable + 1;
+    let b = wrongVariable + 2;
+    let c = wrongVariable + 3;
+    return a + b + c;
+}
+
+// Level 5
+// There is a type in the string being returned, find it and fix it...
+function findAndFixTheTypo(){
+    return "good day kind sir my name is steve and I habe an offer for you on this fine day!";
+}
+
+// Level 6
+// Take the input string and reverse it...
+function reverseTheString(input){
+    return "";
 }
 
 module.exports = {
-  helloName,
-  helloWorld,
-  helloWorldFromArray,
-  doStuffWithVariable,
+    helloName,
+    helloWorld,
+    helloWorldFromArray,
+    doStuffWithVariable,
+    findAndFixTheTypo,
+    reverseTheString,
 };
--- index.test.js
+++ index.test.js
@@ -1,25 +1,35 @@
 const {
-  helloWorld,
-  helloName,
-  helloWorldFromArray,
-  doStuffWithVariable,
+    helloWorld,
+    helloName,
+    helloWorldFromArray,
+    doStuffWithVariable,
+    findAndFixTheTypo,
+    reverseTheString,
 } = require('./index');
 const { expect } = require('chai');
 
 describe('Vim blindfold challenge tests', () => {
-  it('helloWorld', () => {
-    expect(helloWorld()).to.equal("hello world");
-  });
+    it('helloWorld', () => {
+        expect(helloWorld()).to.equal("hello world");
+    });
 
-  it('helloName', () => {
-    expect(helloName("bob")).to.equal("hello bob");
-  });
+    it('helloName', () => {
+        expect(helloName("bob")).to.equal("hello bob");
+    });
 
-  it('helloWorldFromArray', () => {
-    expect(helloWorldFromArray()).to.equal("hello world");
-  });
+    it('helloWorldFromArray', () => {
+        expect(helloWorldFromArray()).to.equal("hello world");
+    });
 
-  it('doStuffWithVariable', () => {
-    expect(doStuffWithVariable(2)).to.equal(12);
-  });
+    it('doStuffWithVariable', () => {
+        expect(doStuffWithVariable(2)).to.equal(12);
+    });
+
+    it('findAndFixTheTypo', () => {
+        expect(findAndFixTheTypo()).to.equal("good day kind sir my name is steve and I have an offer for you on this fine day!");
+    });
+
+    it('reverseTheString', () => {
+        expect(reverseTheString("hats")).to.equal("stah");
+    });
 });
